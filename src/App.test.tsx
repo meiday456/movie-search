82f3833d5a14c7be04dff40e409063ed672dd2e6
meidya456 <sjkang0228@gmail.com>
2023-04-07 22:52:00 +0900

    test 진행시 axios 로 인하여 Cannot use import statement outside a module 발생하는 문제 처리

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("헤더의 title이 출력이 되는가?", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleText = screen.getByText(/OMDbAPI/i);
+  expect(titleText).toBeInTheDocument();
 });
